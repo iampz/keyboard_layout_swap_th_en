847361da05a5d0b88a6965396bfa5d2689ff2ef2
iampz <i@iampz.com>
2012-07-21 15:16:14 +0700

    fix script

--- js/keyboard.js
+++ js/keyboard.js
@@ -1,20 +1,25 @@
 var Keyboard = (function() {
-  var keyboard = {};
-  var thChr = '-ๅ/_ภถุึคตจขชๆไำพะัีรนยบลฃฟหกดเ้่าสวงผปแอิืทมใฝ%+๑๒๓๔ู฿๕๖๗๘๙๐"ฎฑธํ๊ณฯญฐ,ฅฤฆฏโฌ็๋ษศซ.()ฉฮฺ์?ฒฬฦ';
-  var enChr = '`1234567890-=qwertyuiop[]\\asdfghjkl;\'zxcvbnm,./~!@#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:"ZXCVBNM<>?';
-  keyboard.toThai = function(txt) {
-    var newTxt = '';
-    for (var i in txt) {
-      newTxt += thChr.charAt(enChr.indexOf(txt.charAt(i)));
-    }
-    return newTxt;
-  };
-  keyboard.toEng = function(txt) {
-    var newTxt = '';
-    for (var i in txt) {
-      newTxt += enChr.charAt(thChr.indexOf(txt.charAt(i)));
-    }
-    return newTxt;
-  };
-  return keyboard;
+	var keyboard = {};
+	var th = '-ๅ/_ภถุึคตจขชๆไำพะัีรนยบลฃฟหกดเ้่าสวงผปแอิืทมใฝ%+๑๒๓๔ู฿๕๖๗๘๙๐"ฎฑธํ๊ณฯญฐ,ฅฤฆฏโฌ็๋ษศซ.()ฉฮฺ์?ฒฬฦ';
+	var en = '`1234567890-=qwertyuiop[]\\asdfghjkl;\'zxcvbnm,./~!@#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:"ZXCVBNM<>?';
+	var swap = function(txt, from, to) {
+		var chr, pos, newTxt = '';
+		for (var i in txt) {
+			chr = txt.charAt(i);
+			pos = from.indexOf(chr);
+			if (pos !== -1) {
+				newTxt += to.charAt(pos);
+			} else {
+				newTxt += chr;
+			}
+		}
+		return newTxt;
+	}
+	keyboard.toThai = function(txt) {
+		return swap(txt, en, th);
+	};
+	keyboard.toEng = function(txt) {
+		return swap(txt, th, en);
+	};
+	return keyboard;
 })();
